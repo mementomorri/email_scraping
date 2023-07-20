65a4011ed8b2387479dd350d1b23dee0b39f1a5b
mementomorri <44745297+mementomorri@users.noreply.github.com>
2023-07-20 14:45:13 +0300

    initial commit

---
+++ emails_spider.py
@@ -0,0 +1,22 @@
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+import re
+
+
+class EMailsSpider(CrawlSpider):
+    name = "emails"
+    allowed_domains = ["scrapebay.com"]
+    start_urls = ["https://www.scrapebay.com/"]
+
+    rules = (
+        Rule(LinkExtractor(), callback="parse_item",
+             follow=False),
+    )
+
+    @staticmethod
+    def parse_item(response):
+        emails = re.findall(r'[\w.]+@[\w.]+\.[\w]+', response.text)
+
+        for email in emails:
+            if 'bootstrap' not in email:
+                yield {response.url: email}
